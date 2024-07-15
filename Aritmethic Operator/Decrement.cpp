b1997865ec4b80f2378236b50bf5d1671b1956a0
Syarif <154748261+Syharipf@users.noreply.github.com>
2024-07-15 15:53:20 +0700

    Add files via upload

---
+++ Aritmethic Operator/Decrement.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int x;
+
+    cout << "Masukan bilangan bulat: ";
+    cin >> x ;
+
+    int sum = x--;
+
+    cout << "Hasil dari " << sum << "++ " << "adalah " << x;
+
+    return 0;
+
+} 
---
+++ Aritmethic Operator/Pengurangan.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int x, y;
+
+    cout << "Masukan 2 bilangan bulat: ";
+    cin >> x >> y;
+
+    int sum = x - y;
+
+    cout << "Hasil dari " << x << " - " << y << " adalah " << sum;
+
+    return 0;
+
+} 
